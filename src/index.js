420990dc8911232fcd6466785a770344f9be7984
benjaAguilar <benjaaguilar02@gmail.com>
2024-03-13 16:21:45 -0300

    added event listeners to the add section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import css from './styles.css';
+import classTask from './tasks.js'
+import addTasks from './addTasks';
+
+export let allTasks = [];
+
+//DOM
+let showModalBtn = document.querySelector('.addNote');
+let addingModal = document.querySelector('#task-adder');
+let addTaskBtn = document.querySelector('#createTask');
+
+showModalBtn.addEventListener('click', () => {addingModal.showModal()});
+addTaskBtn.addEventListener('click', addTasks);
