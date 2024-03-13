28a2b82f6fd6cc39b699e4b2e204806bde907a47
benjaAguilar <benjaaguilar02@gmail.com>
2024-03-13 16:17:31 -0300

    function addtask
    this function takes the modal info and creates a new task obj

---
+++ src/addTasks.js
@@ -0,0 +1,17 @@
+import classTask from './tasks.js'
+import { allTasks } from './index.js';
+
+export default function addTasks(){
+
+    let title = document.querySelector('#title').value;
+    let description = document.querySelector('#description').value;
+    let duedate = document.querySelector('#duedate').value;
+    let proyect = document.querySelector('#select-proyect').value;
+    let priority = document.querySelector('#select-priority').value;
+
+    if(title != ""){
+        let task = new classTask(title, description, duedate, proyect, priority);
+        allTasks.push(task);
+    }
+
+}
