7144fd61d19b9f378b4da06b681d7cbbd174cc22
benjaAguilar <benjaaguilar02@gmail.com>
2024-03-14 10:54:34 -0300

    added a datepicker to choose the duedate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import css from './styles.css';
 import classTask from './tasks.js'
 import addTasks from './addTasks';
+import { format } from 'date-fns';
+const datepicker = require('js-datepicker')
 
 export let allTasks = [];
 
@@ -8,6 +10,14 @@ export let allTasks = [];
 let showModalBtn = document.querySelector('.addNote');
 let addingModal = document.querySelector('#task-adder');
 let addTaskBtn = document.querySelector('#createTask');
+//datepicker config for duedate
+const picker = datepicker('#duedate', {
+    formatter: (input, date, instance) => {
+        const value = format(date, 'dd MMM yyy');
+        input.value = value;
+    },
+    minDate: new Date(),
+});
 
 showModalBtn.addEventListener('click', () => {addingModal.showModal()});
 addTaskBtn.addEventListener('click', addTasks);
