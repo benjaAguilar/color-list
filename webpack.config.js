8bc6f3de30edba8bc92ded9fe01f3b9a3b6ab3ef
benjaAguilar <benjaaguilar02@gmail.com>
2024-03-20 13:44:23 -0300

    swiched mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js'
